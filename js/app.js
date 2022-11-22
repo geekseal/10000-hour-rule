8bb8cc4db829bb4b49559bc3cab4247890670411
Kihyun Ryu <khryu0610@gmail.com>
2022-11-22 16:58:24 +0900

    feat: 모달창 구현, url 복사 버튼구현, 영어입력 예외처리 구현

--- js/app.js
+++ js/app.js
@@ -5,17 +5,18 @@ const $inpField = $input.querySelector("#inpField");
 const $inpTime = $input.querySelector("#inpTime");
 const $resultField = $result.querySelector("#resultField");
 const $resultTime = $result.querySelector("#resultTime");
+const $modalGo = document.querySelector(".section-modal");
 
 // Buttons
 const $btnSubmit = $input.querySelector(".btn-submit");
-const $btnGo = $input.querySelector(".btn-go");
-const $btnShare = $input.querySelector(".btn-share");
-const $btnClose = $input.querySelector(".btn-close");
+const $btnGo = $result.querySelector(".btn-go");
+const $btnShare = $result.querySelector(".btn-share");
+const $btnClose = document.querySelector(".btn-close");
 
 // 1. 희망 분야 입력 시 영어는 입력되지 않게 하기
 $inpField.addEventListener("input", function (e) {
     setTimeout(() => {
-        this.value = this.value.replace(/[A-Za-z]/gi, "");
+        this.value = this.value.replace(/[A-Za-z0-9]/gi, "");
     }, 400);
 });
 
@@ -39,8 +40,12 @@ function loadResult() {
     if (!$inpField.value || !$inpTime.value) {
         alert("비정상 접근입니다.");
         return;
+    } else if (/[A-Za-z0-9]/gi.test($inpField.value)) {
+        alert("희망 분야는 한글만 입력해주세요.");
+        return;
     } else if ($inpTime.value < 1 || $inpTime.value > 24) {
         alert("하루는 24시간입니다.");
+        $inpTime.value = null;
         return;
     }
 
@@ -63,6 +68,28 @@ function getRequiredDays() {
     return requiredDays;
 }
 
-// 4. 모달창 띄우기
+// 4. 모달창 띄우기, 닫기
+$btnGo.addEventListener("click", openModal);
+function openModal() {
+    $modalGo.style.display = "flex";
+}
+
+$btnClose.addEventListener("click", closeModal);
+function closeModal() {
+    $modalGo.style.display = "none";
+}
+
 // 5. url 복사하기
-// 6. 모달창 닫기
+$btnShare.addEventListener("click", copyUrl);
+function copyUrl() {
+    let url = window.location.href;
+    navigator.clipboard
+        .writeText(url)
+        .then(() => {
+            alert("URL이 복사되었습니다.");
+        })
+        .catch(err => {
+            alert("URL이 복사되지 않았습니다. 호환되는 브라우저가 아닙니다.");
+            console.log(err);
+        });
+}
